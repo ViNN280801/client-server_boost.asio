dc0f297d8f48657cc8f98f2f3d9bb32310ced92a
Vladislav Semykin <vladislav_semykin01@mail.ru>
2023-01-05 11:42:10 +0300

    \'strmanip\' class renamed to \'StringExt\'

--- include/ServerSettings.hpp
+++ include/ServerSettings.hpp
@@ -4,7 +4,7 @@
 #include <syncstream>
 #include <iostream>
 
-#include "strmanip.hpp"
+#include "StringExt.hpp"
 
 static const String auto DEFAULT_IP_ADDRESS_V4{"127.0.0.1"};
 static constexpr std::unsigned_integral auto DEFAULT_PORT_NUMBER{8888U};
