41045cd32c71784768371345d52f2664aba2264f
Vladislav Semykin <vladislav_semykin01@mail.ru>
2023-01-05 10:11:33 +0300

    Concept moved to strmanip.hpp

--- include/ServerSettings.hpp
+++ include/ServerSettings.hpp
@@ -3,10 +3,8 @@
 #include <boost/asio.hpp>
 #include <syncstream>
 #include <iostream>
-#include <concepts>
 
-template <typename T>
-concept String = std::is_convertible_v<T, std::string_view>;
+#include "strmanip.hpp"
 
 static const String auto DEFAULT_IP_ADDRESS_V4{"127.0.0.1"};
 static constexpr std::unsigned_integral auto DEFAULT_PORT_NUMBER{8888U};
