bbb4eabac23e8199cfbcab9356c57bafc71acd66
Vladislav <vladislav_semykin01@mail.ru>
2023-01-12 18:00:14 +0300

    Added validating message-response to clients message

--- src/Server/server.cpp
+++ src/Server/server.cpp
@@ -8,14 +8,26 @@ void Service::handleClient(boost::shared_ptr<boost::asio::ip::tcp::socket> sock)
         {
             boost::asio::streambuf request;
             boost::asio::read_until(*sock.get(), request, '\n');
-            std::string str{asio_streambuf_to_str(request)};
+            StringExt strExt;
+            std::string str{strExt.asio_streambuf_to_str(request)};
 
             // Skipping the idle message
             if (str.at(0UL) not_eq '\n')
-                std::osyncstream(std::cout) << str;
+            {
+                strExt = str;
 
-            std::string response{asio_streambuf_to_str(request)};
-            boost::asio::write(*sock.get(), boost::asio::buffer(response));
+                // Deleting endline symbol -> '\n' and symbol that specifies
+                // that string is ending -> '\0' - nil terminator
+                strExt.removeChars();
+                strExt.removeChars('\0');
+
+                std::osyncstream(std::cout) << "Response to previous clients message: ";
+                (strExt.is_2moreDigits() and strExt.is_32multiple())
+                    ? std::osyncstream(std::cout) << "\033[1;32mData recieved successfully!\033[0m\033[1m\n"
+                    : std::osyncstream(std::cout) << "\033[1;31mError! Data is incorrect\033[0m\033[1m\n";
+            }
+
+            boost::asio::write(*sock.get(), boost::asio::buffer(str));
         }
     }
     catch (const boost::system::system_error &e)
