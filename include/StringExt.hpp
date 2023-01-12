64ed83767ee662372598faf6e7c92c1f6a77f6c7
Vladislav <vladislav_semykin01@mail.ru>
2023-01-12 18:09:46 +0300

    Deleted unnecessary names of args, where implementation is in the .cpp sourse file, added default type to some template funcs

--- include/StringExt.hpp
+++ include/StringExt.hpp
@@ -5,6 +5,9 @@
 #include <ranges>
 #include <algorithm>
 
+#include <boost/asio/streambuf.hpp>
+#include <boost/asio/buffers_iterator.hpp>
+
 template <typename T>
 concept String = std::is_convertible_v<T, std::string_view>;
 
@@ -25,7 +28,7 @@ public:
     explicit StringExt() : m_str("") {}
     explicit StringExt(const String auto &str) : m_str(str){};
     explicit StringExt(const StringExt &obj) : std::string(obj) { copy_obj(obj); }
-    explicit StringExt(StringExt &&rhs);
+    explicit StringExt(StringExt &&);
     virtual ~StringExt() { m_str.clear(); }
 
     constexpr std::string getStr() { return m_str; }
@@ -37,8 +40,9 @@ public:
         return *this;
     }
 
-    StringExt &operator=(const StringExt &obj);
-    StringExt &operator=(StringExt &&rhs) noexcept;
+    StringExt &operator=(const StringExt &);
+    StringExt &operator=(StringExt &&) noexcept;
+
     friend std::ostream &operator<<(std::ostream &os, const StringExt &obj)
     {
         os << obj.m_str;
@@ -54,35 +58,37 @@ public:
     friend inline bool operator==(const StringExt &lhs, const StringExt &rhs) { return strcmp(lhs.c_str(), rhs.c_str()) ? false : true; }
     friend inline bool operator==(const StringExt &lhs, const char *rhs) { return (strcmp(lhs.c_str(), rhs)) ? false : true; }
 
-    StringExt &operator+=(const char &ch);
-    StringExt &operator+=(const char *str);
+    StringExt &operator+=(const char &);
+    StringExt &operator+=(const char *);
 
-    consteval void removeEndl()
+    // Removes all specified occurencies by variable 'charToRemove'
+    constexpr void removeChar(const char &charToRemove = '\n')
     {
-        std::erase_if(m_str, [](const auto &el)
-                      { return el == '\n'; });
+        std::erase_if(m_str, [charToRemove](const auto &ch)
+                      { return ch == charToRemove; });
     }
 
-    template <std::integral T>
+    template <std::unsigned_integral T = uint>
     constexpr T toNumber() const
     {
         std::istringstream iss(m_str);
 
-        T num{};
+        T num{0U};
         iss >> num;
 
         return num;
     }
 
+    constexpr bool isEmpty() const { return m_str.empty(); }
     constexpr bool isLength_lteq_64() const { return m_str.length() <= 64UL ? true : false; }
-    constexpr bool isDigits() const { return std::ranges::all_of(std::ranges::cbegin(m_str), std::ranges::cend(m_str), isdigit); }
-    consteval bool is_32multiple() const { return (toNumber<int>() % 32) ? false : true; }
-    consteval bool is_2moreDigits() const { return (isDigits() and m_str.length() > 2UL) ? true : false; }
+    constexpr bool isDigits() const { return std::ranges::all_of(m_str, isdigit); }
+    constexpr bool is_32multiple() const { return (toNumber() % 32) ? false : true; }
+    constexpr bool is_2moreDigits() const { return (isDigits() and (m_str.length() > 2UL)) ? true : false; }
 
-    consteval virtual void clear() { m_str.clear(); }
-    consteval virtual size_t length() const { return m_str.length(); }
+    constexpr virtual void clear() { m_str.clear(); }
+    constexpr virtual size_t length() const { return m_str.length(); }
 
-    template <std::unsigned_integral T>
+    template <std::unsigned_integral T = uint>
     constexpr T sumOfDigits() const
     {
         T sum{0U};
@@ -99,4 +105,10 @@ public:
     constexpr void sortDescending() { std::ranges::sort(m_str, std::ranges::greater()); }
 
     void replaceEvenDigitsTo_KB();
+
+    inline const std::string asio_streambuf_to_str(const boost::asio::streambuf &buf) const noexcept
+    {
+        return std::string(boost::asio::buffers_begin(buf.data()),
+                           boost::asio::buffers_begin(buf.data()) + buf.size());
+    }
 };
